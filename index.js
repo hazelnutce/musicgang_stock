da9b6ab5485b61d891ba5de4b2f222348865922a
hazelnutce <nutawoot61@gmail.com>
2018-07-11 23:58:55 +0700

    add react front-end side

--- index.js
+++ index.js
@@ -26,6 +26,16 @@ app.use(passport.session())
 require('./routes/testingRoute')(app)
 require('./routes/authRoute')(app)
 
+if(process.env.NODE_ENV === 'production' ){
+    //express will serve up production asset
+    app.use(express.static('client/build'));
+    //express will serve up index.html file if it can't recognize route
+    const path = require('path');
+    app.get('*',(req,res) => {
+      res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
+  }
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log("server're running at PORT 5000 without error :)")
